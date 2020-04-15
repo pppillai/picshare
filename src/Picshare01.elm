88721bbedd6b3b1d66ea6a7173bbe81e4ab3e384
Pradeep Pillai <pradeep.pillai@jumio.com>
2020-04-15 05:41:40 +0530

    state changes

--- src/Picshare01.elm
+++ src/Picshare01.elm
@@ -2,26 +2,86 @@ module Picshare01 exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (class, src)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (class, src, placeholder, type_, disabled, value)
+import Html.Events exposing (onClick, onSubmit, onInput)
 
 
-
-
-initialModel: {url: String, caption: String, liked: Bool}
-initialModel =
+type alias Model =
     {
-        url = baseUrl ++ "1.jpg"
-        , caption = "Surfing"
-        , liked = False
+        url: String
+        , caption: String
+        , liked: Bool
+        , newComment: String
+        , comments: List String
     }
+
+
+type Msg =
+    ToggleLike
+    | UpdateComment String
+    | SaveComment
+
 
 baseUrl : String
 baseUrl =
     "http://localhost:5000/"
 
 
-viewDetailedPhoto : {url: String, caption: String, liked: Bool} -> Html Msg
+initialModel: Model
+initialModel =
+    { url = baseUrl ++ "1.jpg"
+     , caption = "Surfing"
+     , liked = False
+     , newComment = ""
+     , comments = ["Cowobanga Dude!"]
+     }
+
+
+viewComment: String -> Html Msg
+viewComment comment =
+    li []
+    [ strong [] [text "Comment: "]
+    , text comment
+    ]
+
+
+viewCommentList: List String -> Html Msg
+viewCommentList comments =
+    case comments of
+        [] -> text "No Comments!"
+
+        _ ->  ul [] ( List.map viewComment comments )
+
+viewComments: Model -> Html Msg
+viewComments model =
+    div []
+    [
+        viewCommentList model.comments
+        , form [class "new-comment", onSubmit SaveComment]
+            [
+                input [type_ "text", placeholder "Add more comments", value model.newComment, onInput UpdateComment] []
+                , button [disabled (String.isEmpty model.newComment) ] [text "Save"]
+            ]
+    ]
+
+
+viewLoveButton: Model -> Html Msg
+viewLoveButton model =
+    let
+            buttonClass =
+                if model.liked then
+                    "fa-heart"
+                else
+                    "fa-heart-o"
+
+        in
+        div [ class "like-button" ]
+            [
+                i [ class "fa fa-2x" , class buttonClass , onClick ToggleLike] []
+            ]
+
+
+viewDetailedPhoto : Model -> Html Msg
 viewDetailedPhoto model =
     let
         buttonClass =
@@ -30,23 +90,19 @@ viewDetailedPhoto model =
             else
                 "fa-heart-o"
 
-        msg =
-            if model.liked then
-                Unlike
-            else
-                Liked
     in
     div [ class "detailed-photo" ]
         [ img [ src model.url ] []
         , div [ class "photo-info" ]
         [
-            i [ class "fa fa-2x" , class buttonClass , onClick msg] []
-            , h2 [] [ text model.caption ]
+            viewLoveButton model
+            , h2 [class "caption"] [ text model.caption ]
+            , viewComments model
         ]
         ]
 
 
-view : {url: String, caption: String, liked: Bool} -> Html Msg
+view : Model -> Html Msg
 view model =
     div []
         [ div [ class "header" ] [ h1 [] [ text "Picshare" ] ]
@@ -55,20 +111,29 @@ view model =
         ]
 
 
-type Msg =
-    Liked
-    | Unlike
+saveComment: Model -> Model
+saveComment model =
+    let
+        comment = String.trim model.newComment
+    in
+    case comment of
+        "" ->
+            model
+        _ ->
+            { model | comments = model.comments ++ [comment], newComment = "" }
 
 
-update: Msg -> {url: String, caption: String, liked: Bool} -> {url: String, caption: String, liked: Bool}
+
+update: Msg -> Model ->  Model
 update msg model =
     case msg of
-        Liked -> { model | liked = True }
-        Unlike -> { model | liked = False }
+        ToggleLike -> { model | liked = not model.liked }
+        UpdateComment comment -> { model | newComment = comment}
+        SaveComment -> saveComment model
 
 
 
-main: Program () {url: String, caption: String, liked: Bool} Msg
+main: Program () Model Msg
 main =
     Browser.sandbox {
         init = initialModel
